592c9c66f12b07a5522698951650481956600b80
Samuele Guerrini <samuele.guerrini@defenda.io>
2024-09-05 15:28:10 +0200

    language servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,9 +4,23 @@ require("config.lazy")
 vim.api.nvim_set_keymap('n', '<Space>q', ':q<cr>', { noremap = true })
 -- close tab
 vim.api.nvim_set_keymap('n', '<Space>c', ':tabclose<cr>', { noremap = true })
-
+-- line numbers
+vim.cmd('set number')
 
 -- telescope
 local telescopebuiltin = require("telescope.builtin")
 vim.keymap.set('n', '<Space>ff', telescopebuiltin.find_files, {})
 vim.keymap.set('n', '<Space>st', telescopebuiltin.live_grep, {})
+
+-- mason
+require("mason").setup()
+
+-- language servers
+require'lspconfig'.dockerls.setup{} -- Docker
+require'lspconfig'.gopls.setup{} -- Go
+require'lspconfig'.ts_ls.setup{} -- JavaScript/TypeScript
+require'lspconfig'.ltex.setup{} -- LaTeX
+require'lspconfig'.intelephense.setup{} -- PHP
+require'lspconfig'.pyright.setup{} -- Python
+require'lspconfig'.rust_analyzer.setup{} -- Rust
+
