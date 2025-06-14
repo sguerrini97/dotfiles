f54c397bca4bcdee79a50660c49317f9a88b2c26
Samuele Guerrini <sguerrini97@protonmail.com>
2025-06-14 08:49:33 +0200

    install nvchad

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,99 +1,37 @@
-require("config.lazy")
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
 
--- quit
-vim.api.nvim_set_keymap("n", "<Space>q", ":q<cr>", { noremap = true })
--- close tab
-vim.api.nvim_set_keymap("n", "<Space>c", ":tabclose<cr>", { noremap = true })
--- loop over tabs
-vim.api.nvim_set_keymap("n", "<Tab>", ":tabnext<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":tabprevious<cr>", { noremap = true })
--- open lazygit in floaterm
-vim.api.nvim_set_keymap("n", "<Space>gg", ":FloatermNew --width=0.96 --height=0.96 lazygit<cr>", { noremap = true })
--- show line numbers
-vim.cmd("set number")
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- telescope
-local telescopebuiltin = require("telescope.builtin")
-vim.keymap.set("n", "<Space>ff", telescopebuiltin.find_files, {})
-vim.keymap.set("n", "<Space>st", telescopebuiltin.live_grep, {})
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
 
--- add cmp_nvim_lsp capabilities to lspconfig
--- should run before any language server configuration
-local lspconfig_defaults = require("lspconfig").util.default_config
-lspconfig_defaults.capabilities =
-	vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+vim.opt.rtp:prepend(lazypath)
 
--- mason language server autosetup
-require("mason").setup({})
-require("mason-lspconfig").setup({
-	handlers = {
-		function(server_name)
-			if server_name == "lua_ls" then
-				require("lspconfig").lua_ls.setup({
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			else
-				require("lspconfig")[server_name].setup({})
-			end
-		end,
-	},
-})
+local lazy_config = require "configs.lazy"
 
--- set options
-local set = vim.opt
-set.tabstop = 2
-set.softtabstop = 2
-set.shiftwidth = 2
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
 
--- color scheme
-vim.opt.termguicolors = true
-vim.cmd.colorscheme("tokyonight-night")
+  { import = "plugins" },
+}, lazy_config)
 
--- reserve a space in the gutter
-vim.opt.signcolumn = "yes"
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
--- enable features for when there is a language server active on the file
-vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP actions",
-	callback = function(event)
-		local opts = { buffer = event.buf }
+require "options"
+require "autocmds"
 
-		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	end,
-})
-
--- completion
-local cmp = require("cmp")
-
-cmp.setup({
-	sources = {
-		{ name = "nvim_lsp" },
-	},
-	mapping = cmp.mapping.preset.insert({
-		-- Enter to confirm completion
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-		-- Ctrl+Space to trigger completion menu
-		["<C-Space>"] = cmp.mapping.complete(),
-	}),
-	snippet = {
-		expand = function(args)
-			vim.snippet.expand(args.body)
-		end,
-	},
-})
+vim.schedule(function()
+  require "mappings"
+end)
--- .config/nvim/lua/plugins/lsp-zero.lua
+++
@@ -1,5 +0,0 @@
-return {
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v4.x" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/nvim-cmp" },
-}
