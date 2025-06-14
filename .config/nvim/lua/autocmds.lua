977d19c7219531448af37b030494c8d549313a13
Samuele Guerrini <sguerrini97@protonmail.com>
2025-06-14 09:14:00 +0200

    add nvim format on save, no gui bg

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,1 +1,11 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
+
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
