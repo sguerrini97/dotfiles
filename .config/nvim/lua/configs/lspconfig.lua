2b2a238e444be0c9726cbef43c6324b5f13e3bbf
Samuele Guerrini <samuele.guerrini@defenda.io>
2025-06-16 15:08:26 +0200

    add intelephense to lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls", "gofumpt" }
+local servers = { "html", "cssls", "gopls", "gofumpt", "intelephense" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
