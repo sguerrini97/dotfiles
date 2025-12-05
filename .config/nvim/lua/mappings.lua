0f51a0269d028d31af95391af01088b2d878c607
Samuele Guerrini <samuele.guerrini@defenda.io>
2025-12-05 12:32:33 +0100

    add mappings to show diagnostic messages in nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "dN", function()
+  vim.diagnostic.goto_prev()
+end, opts)
+map("n", "dn", function()
+  vim.diagnostic.goto_next()
+end, opts)
+map("n", "dm", function()
+  vim.diagnostic.open_float()
+end, opts)
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
